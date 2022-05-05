9a269cc8dfd2cc958409915ac9d95839986422c8
Rafael Freitas <rafaelcerqueira92@gmail.com>
2022-05-05 19:39:22 -0300

    Implementando Front-End | Consulta de CEP

--- src/App-generic.js
+++ src/App-generic.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import Main from "./components/Main";
 import Form from "./components/Form";
-import Label from "./components/Field/Label";
+import Main from "./components/Main";
 
 
 function App() {
@@ -11,14 +10,11 @@ function App() {
 
   const onSubmit = (e) => {
     console.log(e);
-
   }
-  
+
   const buscarCep = (e) => {
     const cep = e.target.value.replace(/\D/g, '');
-    console.log(cep)
-
-    if(!e.target.value) return;
+    console.log(cep);
 
     fetch(`https://viacep.com.br/ws/${cep}/json/ `)
     .then(res => res.json())
@@ -30,36 +26,34 @@ function App() {
       setValue('uf', data.uf);
       setFocus('uf');
     })
-    .catch((error) => console.log(error));
   }
 
   return (
     <Main>
-      <Form onSubmit={handleSubmit(onSubmit)} >
       <h1>Consultar CEP</h1>
-        
-        <Label>
-          <span>CEP:</span>
-          <input type="text" {...register("cep")} onBlur={buscarCep} />
-        </Label>
-        <Label>
-          <span>Logradouro:</span>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          CEP:
+          <input type="text" {...register("cep")} onBlur={buscarCep}/>
+        </label>
+        <label>
+          Logradouro:
           <input type="text" {...register("logradouro")} />
-        </Label>
-        <Label>
-          <span>Bairro:</span>
+        </label>
+        <label>
+          Bairro:
           <input type="text" {...register("bairro")} />
-        </Label>
-        <Label>
-          <span>Cidade:</span>
+        </label>
+        <label>
+          Cidade:
           <input type="text" {...register("localidade")} />
-        </Label>
-        <Label>
-          <span>UF:</span>
+        </label>
+        <label>
+          UF:
           <input type="text" {...register("uf")} />
-        </Label>
-        <button type="submit" >
-          Buscar
+        </label>
+        <button>
+          Pesquisar
         </button>
       </Form>
     </Main>
